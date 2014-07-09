fc5914c849a35e44a4a6f95fc72af8561a0b974b
Jesus Marquez <jamarquez@uao.edu.co>
2014-07-09 16:18:42 -0500

    add caso de uso ver actividades

--- src/Ingenieria/ProfesorBundle/Controller/DefaultController.php
+++ src/Ingenieria/ProfesorBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Ingenieria\ProfesorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ingenieria\Estudiante\Entity\Estudiante;
+use Ingenieria\Profesor\Entity\Actividad;
 
 class DefaultController extends Controller
 {
@@ -53,6 +54,28 @@ class DefaultController extends Controller
 		return $this->render('IngenieriaProfesorBundle:Default:estudiantes.html.twig', array('listaEstudiantes' => $listaEstudiantes, 'msgerr' => $msgerr));
 	}
 
+	//********************************************************
+	// Muestra un listado de actividades del profesor
+	//******************************************************** 	
+	public function actividadesAction(){
+		$repository = $this->getDoctrine()->getRepository('IngenieriaProfesorBundle:Actividad');
+		$listaActividades = $repository->findAll();
+		
+		if (!$listaActividades) {
+			$msgerr = array('descripcion'=>'No hay actividades registradas!','id'=>'1');
+		}else{
+			$msgerr = array('descripcion'=>'','id'=>'0');
+		}
+		/*
+				//buscamos el programa
+		$user = $this->get('security.context')->getToken()->getUser();
+		$coordinador =  $user->getUsername();
+		$repository = $this->getDoctrine()->getRepository('CituaoUsuarioBundle:Programa');
+		$programa = $repository->findOneByCoordinador($coordinador);
+		*/
+		
+		return $this->render('IngenieriaProfesorBundle:Default:actividades.html.twig', array('listaActividades' => $listaActividades, 'msgerr' => $msgerr));
+	}
 
 	
 }
