9f29df4bb8b3a9a1023bf3a955b08e299989901c
unknown <jamarquez@AUCAAANP4D47794.uao.edu.co>
2014-07-09 14:05:33 -0500

    add - title modulo

--- src/Ingenieria/ProfesorBundle/Controller/DefaultController.php
+++ src/Ingenieria/ProfesorBundle/Controller/DefaultController.php
@@ -9,7 +9,25 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('IngenieriaProfesorBundle:Default:index.html.twig');
+ 		$repository = $this->getDoctrine()->getRepository('IngenieriaEstudianteBundle:Estudiante');
+		$listaEstudiantes = $repository->findAll();
+		
+		if (!$listaEstudiantes) {
+			$msgerr = array('descripcion'=>'No hay estudiantes registrados!','id'=>'1');
+		}else{
+			$msgerr = array('descripcion'=>'','id'=>'0');
+		}
+		/*
+				//buscamos el programa
+		$user = $this->get('security.context')->getToken()->getUser();
+		$coordinador =  $user->getUsername();
+		$repository = $this->getDoctrine()->getRepository('CituaoUsuarioBundle:Programa');
+		$programa = $repository->findOneByCoordinador($coordinador);
+		*/
+		
+		return $this->render('IngenieriaProfesorBundle:Default:estudiantes.html.twig', array('listaEstudiantes' => $listaEstudiantes, 'msgerr' => $msgerr));
+       
+		
     }
 
 	//********************************************************
