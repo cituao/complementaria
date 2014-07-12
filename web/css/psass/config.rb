3e1a32141361b68c27665daa23ee8369f1cff7ab
Jesus Marquez <jesmarquezacevedo@yahoo.es>
2014-07-12 17:57:46 -0500

    fix sass project

--- web/css/psass/config.rb
+++ web/css/psass/config.rb
@@ -1,15 +1,11 @@
-require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-css_path = "C:/xampp/htdocs/practicas/web/css"
+css_dir = "../../css"
 sass_dir = "sass"
 images_dir = "images"
-images_path = "C:/xampp/htdocs/practicas/web/img"
 javascripts_dir = "javascripts"
-javascripts_path = "C:/xampp/htdocs/practicas/web/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
