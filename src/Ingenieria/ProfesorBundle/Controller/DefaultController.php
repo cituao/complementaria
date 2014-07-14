6721712fcd812be6fdcb4062cfc703ea1bbebc54
jamarquez <jesmarquezacevedo@yahoo.es>
2014-07-14 15:39:15 -0500

    fix ver estudiantes profesor

--- src/Ingenieria/ProfesorBundle/Controller/DefaultController.php
+++ src/Ingenieria/ProfesorBundle/Controller/DefaultController.php
@@ -37,23 +37,24 @@ class DefaultController extends Controller
 	// Muestra un listado de estudiantes
 	//******************************************************** 	
 	public function estudiantesAction(){
-		$repository = $this->getDoctrine()->getRepository('IngenieriaEstudianteBundle:Estudiante');
-		$listaEstudiantes = $repository->findAll();
+		//localizamos al profesor
+		$user = $this->get('security.context')->getToken()->getUser();
+		$ci =  $user->getUsername();
+		$repository = $this->getDoctrine()->getRepository('IngenieriaProfesorBundle:Profesor');
+		$profesor = $repository->findOneBy(array('ci' => $ci));
+
+		//$repository = $this->getDoctrine()->getRepository('IngenieriaProfesorBundle:Actividad');
+		//$listaActividades = $repository->findAll();
 		
-		if (!$listaEstudiantes) {
-			$msgerr = array('descripcion'=>'No hay estudiantes registrados!','id'=>'1');
+		$listaActividades =  $profesor->getActividades();
+		if ( $listaActividades == null) {
+			$msgerr = array('descripcion'=>'No hay actividades registradas!','id'=>'1');
 		}else{
 			$msgerr = array('descripcion'=>'','id'=>'0');
 		}
-		/*
-				//buscamos el programa
-		$user = $this->get('security.context')->getToken()->getUser();
-		$coordinador =  $user->getUsername();
-		$repository = $this->getDoctrine()->getRepository('CituaoUsuarioBundle:Programa');
-		$programa = $repository->findOneByCoordinador($coordinador);
-		*/
+
 		
-		return $this->render('IngenieriaProfesorBundle:Default:estudiantes.html.twig', array('listaEstudiantes' => $listaEstudiantes, 'msgerr' => $msgerr));
+		return $this->render('IngenieriaProfesorBundle:Default:estudiantes.html.twig', array('listaActividades' => $listaActividades, 'msgerr' => $msgerr));
 	}
 
 	//********************************************************
