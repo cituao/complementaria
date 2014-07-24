00b535d1e3941753048a53402168f399ce4a9ab6
jamarquez <jesmarquezacevedo@yahoo.es>
2014-07-24 11:30:27 -0500

    add entidad categoria creada

---
+++ src/Ingenieria/UsuarioBundle/Entity/Categoria.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ingenieria\UsuarioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categoria
+ */
+class Categoria
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Categoria
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
