7353cdcf9d32d81c74a33e50212de5f3d99df8bc
Jesus Marquez <jesmarquezacevedo@yahoo.es>
2015-08-31 08:27:20 -0500

    fix increase field size descripcion in bitacora

--- src/Ingenieria/EstudianteBundle/Form/Type/BitacoraType.php
+++ src/Ingenieria/EstudianteBundle/Form/Type/BitacoraType.php
@@ -11,7 +11,7 @@ class BitacoraType extends AbstractType
     {
         $builder
 		->add('nombreActividad','text', array('label' => 'Nombre actividad:' , 'required' => true))
-		->add('descripcion','textarea', array('label' => 'Descripción:' , 'required' => true , 'attr' => array('cols' => '60', 'rows' => '7')))
+		->add('descripcion','textarea', array('label' => 'Descripción:' , 'required' => true , 'attr' => array('cols' => '60', 'rows' => '7', 'placeholder' => '¡Máximo 1200 carácteres!' )))
 		->add('fechaInicio', 'date', array('label' => 'Fecha inicio:','widget' => 'single_text',  'format' => 'dd-MM-yyyy', 'read_only' => 'true'))
 		->add('fechaFin', 'date', array('label' => 'Fecha fin','widget' => 'single_text',  'format' => 'dd-MM-yyyy', 'read_only' => 'true'))
 		->add('finalizada', 'checkbox', array('required' => false, 'label' => '¿Finalizado?:'));
